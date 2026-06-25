8bae352cef74e7332b8d8dfb9f2ec39784260530
Teleporter998 <Frog567TerdoArt1236@gmail.com>
2026-06-25 02:17:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-(function () {
-  const glow = document.getElementById('glow');
 
-  // A simple, lightweight cursor tracker for the background light
-  window.addEventListener('mousemove', (e) => {
-    if (glow) {
-      // Use requestAnimationFrame for smooth performance
-      requestAnimationFrame(() => {
-        glow.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
-      });
-    }
-  });
-})();
